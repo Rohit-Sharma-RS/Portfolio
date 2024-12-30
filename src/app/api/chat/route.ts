104dabfe9312098b1c811a9d3898f711b74582e0
Rohit Sharma <rohitrnc5458@gmail.com>
2024-12-30 22:52:44 +0530

    pushing

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,81 +1,117 @@
-// import { getVectorStore } from "../../../lib/vectordb";
-// import { AIMessage, HumanMessage } from "@langchain/core/messages";
-// import {
-//   ChatPromptTemplate,
-//   MessagesPlaceholder,
-//   PromptTemplate,
-// } from "@langchain/core/prompts";
-// import { LangChainStream, Message, StreamingTextResponse } from "ai";
-// import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
-// import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
-// import { createRetrievalChain } from "langchain/chains/retrieval";
-// // import { HuggingFaceHub } from "langchain/llms/huggingface_hub";
+import { getVectorStore } from "@/lib/vectordb";
+import { UpstashRedisCache } from "@langchain/community/caches/upstash_redis";
+import { AIMessage, HumanMessage } from "@langchain/core/messages";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+  PromptTemplate,
+} from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { Redis } from "@upstash/redis";
+import { LangChainStream, Message, StreamingTextResponse } from "ai";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
+import { createRetrievalChain } from "langchain/chains/retrieval";
 
-// export async function POST(req: Request) {
-//   try {
-//     const body = await req.json();
-//     const messages = body.messages;
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const messages = body.messages;
 
-//     const latestMessage = messages[messages.length - 1].content;
+    const latestMessage = messages[messages.length - 1].content;
 
-//     const { stream, handlers } = LangChainStream();
+    const { stream, handlers } = LangChainStream();
 
-//     // Use HuggingFace-based chatbot
-//         // const chatModel = new HuggingFaceInference({
-//         //   model: "facebook/blenderbot-400M-distill", // Free chatbot model
-//         //   streaming: true,
-//         //   callbacks: [handlers],
-//         //   verbose: true,
-//         //   temperature: 0.7,
-//         // });
+    // store the same user questions
+    const cache = new UpstashRedisCache({
+      client: Redis.fromEnv(),
+    });
 
-//     const retriever = (await getVectorStore()).asRetriever();
+    const chatModel = new ChatOpenAI({
+      model: "gpt-3.5-turbo-0125",
+      streaming: true,
+      callbacks: [handlers],
+      verbose: true, // logs to console
+      cache,
+      temperature: 0,
+    });
 
-//     // Retrieve context based on chat history
-//     const chatHistory = messages
-//       .slice(0, -1) // Exclude the latest message
-//       .map((msg: Message) =>
-//         msg.role === "user"
-//           ? new HumanMessage(msg.content)
-//           : new AIMessage(msg.content),
-//       );
+    const rephraseModel = new ChatOpenAI({
+      model: "gpt-3.5-turbo-0125",
+      verbose: true,
+      cache,
+    });
 
-//     // Final prompt configuration
-//     const prompt = ChatPromptTemplate.fromMessages([
-//       [
-//         "system",
-//         "You are a friendly chatbot for a developer's personal portfolio website. " +
-//           "Help users by answering questions based on the given context below. " +
-//           "Provide concise responses in markdown format.\n\n" +
-//           "Context:\n{context}",
-//       ],
-//       new MessagesPlaceholder("chat_history"),
-//       ["user", "{input}"],
-//     ]);
+    const retriever = (await getVectorStore()).asRetriever();
 
-//     const combineDocsChain = await createStuffDocumentsChain({
-//       llm: chatModel,
-//       prompt,
-//       documentPrompt: PromptTemplate.fromTemplate(
-//         "Page content:\n{page_content}",
-//       ),
-//       documentSeparator: "\n------\n",
-//     });
+    // get a customised prompt based on chat history
+    const chatHistory = messages
+      .slice(0, -1) // ignore latest message
+      .map((msg: Message) =>
+        msg.role === "user"
+          ? new HumanMessage(msg.content)
+          : new AIMessage(msg.content),
+      );
 
-//     // Retrieval chain setup
-//     const retrievalChain = await createRetrievalChain({
-//       combineDocsChain,
-//       retriever: retriever,
-//     });
+    const rephrasePrompt = ChatPromptTemplate.fromMessages([
+      new MessagesPlaceholder("chat_history"),
+      ["user", "{input}"],
+      [
+        "user",
+        "Given the above conversation history, generate a search query to look up information relevant to the current question. " +
+          "Do not leave out any relevant keywords. " +
+          "Only return the query and no other text. ",
+      ],
+    ]);
 
-//     retrievalChain.invoke({
-//       input: latestMessage,
-//       chat_history: chatHistory,
-//     });
+    const historyAwareRetrievalChain = await createHistoryAwareRetriever({
+      llm: rephraseModel,
+      retriever,
+      rephrasePrompt,
+    });
 
-//     return new StreamingTextResponse(stream);
-//   } catch (error) {
-//     console.error(error);
-//     return Response.json({ error: "Internal server error" }, { status: 500 });
-//   }
-// }
+    // final prompt
+    const prompt = ChatPromptTemplate.fromMessages([
+      [
+        "system",
+        "You are Ro Support, a friendly chatbot for Rohit's personal developer portfolio website. " +
+          "You are trying to convince potential employers to hire Rohit as a software developer. " +
+          "Be concise and only answer the user's questions based on the provided context below. " +
+          "Provide links to pages that contains relevant information about the topic from the given context. " +
+          "Format your messages in markdown.\n\n" +
+          "Context:\n{context}",
+      ],
+      new MessagesPlaceholder("chat_history"),
+      ["user", "{input}"],
+    ]);
+
+    const combineDocsChain = await createStuffDocumentsChain({
+      llm: chatModel,
+      prompt,
+      documentPrompt: PromptTemplate.fromTemplate(
+        "Page content:\n{page_content}",
+      ),
+      documentSeparator: "\n------\n",
+    });
+
+    // 1. retrievalChain converts the {input} into a vector
+    // 2. do a similarity search in the vector store and finds relevant documents
+    // 3. pairs the documents to createStuffDocumentsChain and put into {context}
+    // 4. send the updated prompt to chatgpt for a customised response
+
+    const retrievalChain = await createRetrievalChain({
+      combineDocsChain,
+      retriever: historyAwareRetrievalChain, // get the relevant documents based on chat history
+    });
+
+    retrievalChain.invoke({
+      input: latestMessage,
+      chat_history: chatHistory,
+    });
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
